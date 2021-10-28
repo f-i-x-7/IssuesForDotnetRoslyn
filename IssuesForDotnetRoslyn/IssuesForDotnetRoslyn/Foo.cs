46dff6f216b88367cb1feee7c4130a8041c2ed9e
Artem Eliseev <64615981+f-i-x-7@users.noreply.github.com>
2021-10-28 12:59:12 +0300

    Added sample for NRE false positive at .NET Standard 2.0.

---
+++ IssuesForDotnetRoslyn/IssuesForDotnetRoslyn/Foo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IssuesForDotnetRoslyn
+{
+    public sealed class Foo<T>
+    {
+        private readonly Func<int, T>? _func1;
+        private readonly Func<T>? _func2;
+
+        public Foo(Func<int, T>? func)
+        {
+            _func1 = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        public Foo(Func<T>? func)
+        {
+            _func2 = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        public T Bar()
+        {
+            if (_func1 is not null)
+            {
+                return _func1(42);
+            }
+
+            Debug.Assert(_func2 is not null); // Should eliminate warning CS8602 Dereference of a possibly null reference
+            return _func2(); // But actually warning is shown for .NET Standard 2.0
+        }
+    }
+}
